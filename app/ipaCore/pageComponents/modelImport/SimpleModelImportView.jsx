ca149d61b135674977077b2280619af395734241
scottmollon-invicara <scott.mollon@invicara.com>
2025-02-09 11:03:58 -0500

    disable uploading of Navis models

--- app/ipaCore/pageComponents/modelImport/SimpleModelImportView.jsx
+++ app/ipaCore/pageComponents/modelImport/SimpleModelImportView.jsx
@@ -23,7 +23,8 @@ import {
   validateFile,
 } from './helpers';
 
-const VALID_FILE_EXT = ['.bimpk', '.sgpk'];
+// navis exports are no currently supported in quick model view so do not allow sgpk files to be uploaded
+const VALID_FILE_EXT = ['.bimpk'/*, '.sgpk'*/];
 
 class SimpleModelImportView extends React.Component {
 
