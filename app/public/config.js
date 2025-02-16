60aba72f9540025a5d4f1b33e294ec5b11bd3be8
scottmollon-invicara <scott.mollon@invicara.com>
2025-02-16 16:04:51 -0500

    switching to serving dev server on 8083 as those are the redirects we current create by default for ew applications

--- app/public/config.js
+++ app/public/config.js
@@ -4,6 +4,6 @@ const endPointConfig = {
                            fileServiceOrigin: 'https://sandbox-api.invicara.com',
                            datasourceServiceOrigin: 'https://sandbox-api.invicara.com',
                            graphicsServiceOrigin: 'https://sandbox-api.invicara.com',
-                           baseRoot: 'http://localhost:8084',
+                           baseRoot: 'http://localhost:8083',
                            applicationId: '859f8b75-0114-4f70-9edf-8fa8b8e640e5'
                         }
