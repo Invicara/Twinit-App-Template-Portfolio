f9332372d1474d2f6958bdf47f803290093dd9e6
scottmollon-invicara <scott.mollon@invicara.com>
2025-02-27 12:57:42 -0500

    basic data table reszing in bottom panel

--- app/ipaCore/pageComponents/simpleViewer/panels/TablePanel.jsx
+++ app/ipaCore/pageComponents/simpleViewer/panels/TablePanel.jsx
@@ -1,20 +1,80 @@
 import React, { useEffect, useState, useContext } from 'react'
 
 import { CompactTable } from '@table-library/react-table-library/compact'
+import { useTheme } from "@table-library/react-table-library/theme"
+import { getTheme } from "@table-library/react-table-library/baseline"
 
 import { ModelContext } from '../SimpleViewerView'
 
 import './TablePanel.scss'
+
+const COLORS = {
+   FONT_PRIMARY: '#141414',
+   FONT_SECONDARY: '#757575',
+   FONT_DISABLED: '#9e9e9e',
+   BORDER: '#e0e0e0'
+}
+
+const BASELINE_THEME = {
+   Table: '',
+   Header: '',
+   Body: '',
+   BaseRow: `
+     font-size: 16px;
+   `,
+   HeaderRow: `
+     color: ${COLORS.FONT_PRIMARY};
+   `,
+   Row: `
+     color: ${COLORS.FONT_SECONDARY};
+ 
+     &.disabled {
+       color: ${COLORS.FONT_DISABLED};
+     }
+ 
+     &:hover {
+       color: ${COLORS.FONT_PRIMARY};
+     }
+ 
+     &:not(:last-of-type) > .td {
+       border-bottom: 1px solid ${COLORS.BORDER};
+     }
+   `,
+   BaseCell: `
+     padding: 6px 12px;
+   `,
+   HeaderCell: `
+     font-weight: bold;
+     border-bottom: 1px solid ${COLORS.BORDER};
+ 
+     .resizer-handle {
+       background-color: ${COLORS.BORDER};
+     }
+ 
+     svg,
+     path {
+       fill: currentColor;
+     }
+   `,
+   Cell: `
+     &:focus {
+       outline: dotted;
+       outline-width: 1px;
+       outline-offset: -1px;
+     }
+   `,
+ }
 
 const TablePanel = () => {
 
    const { selectedPropRefs, sliceElements } = useContext(ModelContext)
 
    const [ columns, setColumns ] = useState([])
+   const [ theme, setTheme ] = useState()
 
    useEffect(() => {
 
-      if (selectedPropRefs.length && sliceElements.length) getColumns()
+      if (selectedPropRefs.length && sliceElements.length) getTableConfig()
 
    }, [selectedPropRefs, sliceElements])
 
@@ -32,29 +92,39 @@ const TablePanel = () => {
 
    }
 
-   const getColumns = () => {
+   const getTableConfig = () => {
 
       setColumns([])
 
-      // rerender was happening too fast and running into a ubug with
-      // CompactTable, whre removing a column was not recalculating the
-      // number of columns, and displaying too may columns
-      setTimeout(() => {
-         let columns = [{
-            label: '_id', renderCell: (item) => item._id
-         }]
-   
-         columns.push(...selectedPropRefs.map(spr => {
-            return {
-               label: spr.property.dName,
-               renderCell: (item) => getPropertyValueIfExists(spr, item)
-            }
-         }))
-   
-         setColumns(columns)
-      }, 1000)
 
-      
+      let columns = [{
+         label: '_id', renderCell: (item) => item._id
+      }]
+
+      columns.push(...selectedPropRefs.map(spr => {
+         return {
+            label: spr.property.dName,
+            renderCell: (item) => getPropertyValueIfExists(spr, item)
+         }
+      }))
+
+      let baselineTheme = getTheme
+
+      console.log(baselineTheme)
+      console.log(typeof baselineTheme)
+
+
+      let theme = useTheme([
+         BASELINE_THEME,
+         {
+           Table: `
+             --data-table-library_grid-template-columns: 20% repeat(${columns.length-1}, min-content);
+           `,
+         },
+       ])
+
+      setTheme(theme)
+      setColumns(columns)
 
    }
 
@@ -64,10 +134,11 @@ const TablePanel = () => {
       </div>}
       {!!columns.length && <div className='table-wrapper'><CompactTable
          columns={columns}
+         theme={theme}
          data={{nodes: sliceElements}}
+         layout={{ custom: true, horizontalScroll: true }}
       /></div>}
    </div>
-
 
 }
 
