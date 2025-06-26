72d2015deb4ac6faf19fa0151e9531920410f857
ibrahimfirat-invicara <ibrahim.firat@invicara.com>
2025-06-26 17:03:49 +0100

    Initial work on file item relation

--- app/ipaCore/components/ModelDocs/components/ModelDocUpload.jsx
+++ app/ipaCore/components/ModelDocs/components/ModelDocUpload.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react"
 
-import { IafFileSvc, IafProj } from "@dtplatform/platform-api"
+import { IafFileSvc, IafProj, IafFile, IafItemSvc } from "@dtplatform/platform-api"
+import { IafScriptEngine } from '@dtplatform/iaf-script-engine'
 
 import { ModelContext } from "../../../contexts/ModelContext"
 
@@ -8,19 +9,18 @@ import { getModelFolder } from "../modelDocUtils"
 import { ChooseFiles } from "../../ChooseFiles/ChooseFiles"
 
 const ModelDocUpload = ({ onFilesUploaded }) => {
+	const { selectedModelComposite, selectedElement, modelRelatedCollections } = useContext(ModelContext)
 
-   const { selectedModelComposite } = useContext(ModelContext)
+	const [busy, setBusy] = useState(false)
+	const [totalFileCount, setTotalFileCount] = useState(0)
+	const [currentFileCount, setCurrentFileCount] = useState()
+	const [currentProgress, setCurrentProgress] = useState()
 
-   const [ busy, setBusy ] = useState(false)
-   const [ totalFileCount, setTotalFileCount ] = useState(0)
-   const [ currentFileCount, setCurrentFileCount ] = useState()
-   const [ currentProgress, setCurrentProgress ] = useState()
+	const uploadFilesToModelFolder = async (project, folder, files) => {
 
-   const uploadFilesToModelFolder = async (project, folder, files) => {
+		let fileUploadResults = []
 
-      let fileUploadResults = []
-
-      // we will provide an onComplete callback to the upload function so that we can resolve
+		// we will provide an onComplete callback to the upload function so that we can resolve
 		// our deferred Promises once the upload completes
 		// we'll have this print to the console and the scripts callback
 		// you will see the scripts callback content in the script results when the script
@@ -30,7 +30,7 @@ const ModelDocUpload = ({ onFilesUploaded }) => {
 			let message = file._name + ' COMPLETE'
 			console.log(message)
 			fileUploadResults.push(file)
-         setCurrentProgress('100')
+			setCurrentProgress('100')
 			deferredResolve()
 		}
 
@@ -40,7 +40,7 @@ const ModelDocUpload = ({ onFilesUploaded }) => {
 		// has completed running
 		// the callback you provide for onProgress will be passed the bytes uploaded so far the total bytes
 		function onUploadProgress(bytesUploaded, bytesTotal, file) {
-			let percentComplete = (bytesUploaded/bytesTotal * 100).toFixed(1)
+			let percentComplete = (bytesUploaded / bytesTotal * 100).toFixed(1)
 			let message = file.name + ': ' + percentComplete
 			setCurrentProgress(percentComplete)
 		}
@@ -55,49 +55,65 @@ const ModelDocUpload = ({ onFilesUploaded }) => {
 			deferredReject(error)
 		}
 
+		const sharedContainer = (await IafFile.getContainers(project, { _name: 'SHARED' }))[0];
+
 		// upload each file
 		for (let i = 0; i < files.length; i++) {
 
-         setCurrentFileCount(i+1)
-         let file = files[i]
+			setCurrentFileCount(i + 1)
+			let file = files[i]
 
 			// since the file will be uploaded async we create a Promise and only resolve it once the file
 			// has been 100% uploaded, making sure that the script does not complete before that.
 			// We will pass the deferred resolve method to the onUploadComplete callback
 			let deferredResolve, deferredReject
-			let uploadPromise =new Promise((resolve, reject) => {
+			let uploadPromise = new Promise((resolve, reject) => {
 				deferredResolve = resolve
 				deferredReject = reject
 			})
 
 			try {
+				await IafFile.uploadFileResumable(sharedContainer, file, {
+					filename: encodeURI(file.name),
+					onProgress: (bytesUploaded, bytesTotal) => onUploadProgress(bytesUploaded, bytesTotal, file),
+					onComplete: (upfile) => onUploadComplete(deferredResolve, upfile), // onComplete will be passed the file record in the file service
+					onError: (error) => onUploadError(deferredReject, error, file)
+				});
 
-				// upload the file using resumable upload which can handle interrupts in network and which
-				// will allow partial file uploads that can be completed at a later point in time
-				//
-				// Params:
-				// 1. the file Stream, Buffer, or File to upload
-				// 2. the project _namespaces to which to upload the files
-				// 3. the parent folders for the file, if none are provided the root folder for the project will be used
-				// 4. the tags to apply to the file
-				// 5. the user's ctx uploading the files
-				// 6. an options object containing
-				// 	the filename for the file if not provided on the file
-				//		onProgress, onComplete, and onError options callbacks
-				//
-				// We will upload one file at a time, but you can do parallel uploads by removing await
-				// and throttling the number of uploads you allow at one time
+				await Promise.all([uploadPromise]);
 
-				await IafFileSvc.addFileResumable(file, project._namespaces, [folder._id], [], null, {
-						filename: encodeURI(file.name),
-						onProgress: (bytesUploaded, bytesTotal) => onUploadProgress(bytesUploaded, bytesTotal, file),
-						onComplete: (upfile) => onUploadComplete(deferredResolve, upfile), // onComplete will be passed the file record in the file service
-						onError: (error) => onUploadError(deferredReject, error, file)
+				const relations = [
+					{
+						_relatedFromId: selectedElement._id,
+						_relatedToIds: [fileUploadResults[0]._id],
+						_relatedUserItemDbId: sharedContainer._userItemId,
 					}
-				)
+				];
 
-            await Promise.all(uploadPromise)
-			} catch(e) {
+				await IafItemSvc.addRelations(modelRelatedCollections.elements._userItemId, relations);
+
+				const result = await IafScriptEngine.findWithRelated({
+					parent: {
+						query: {
+							_id: selectedElement._id
+						},
+						collectionDesc: {
+							_userItemId: modelRelatedCollections.elements._userItemId,
+						}
+					},
+					related: [
+						{
+							relatedDesc: {
+								_relatedUserType: "file_container"
+							},
+							query: {},
+							as: "RelatedFiles"
+						}
+					]
+				});
+
+				console.log({ result })
+			} catch (e) {
 				console.log(e)
 				deferredReject(e)
 			}
@@ -105,37 +121,37 @@ const ModelDocUpload = ({ onFilesUploaded }) => {
 		}
 
 		// wait for the onUploadSuccess callbacks to resolve all our deferred Promises then return from the script
-      return fileUploadResults
+		return fileUploadResults
 
-   }
+	}
 
-   const uploadFiles = async (files) => {
+	const uploadFiles = async (files) => {
 
-      if (files.length) {
+		if (files.length) {
 
-         setTotalFileCount(files.length)
-         setCurrentFileCount(0)
-         setCurrentProgress('0')
-         setBusy(true)
+			setTotalFileCount(files.length)
+			setCurrentFileCount(0)
+			setCurrentProgress('0')
+			setBusy(true)
 
-         let currentProject = await IafProj.getCurrent()
+			let currentProject = await IafProj.getCurrent()
 
-         getModelFolder(currentProject, selectedModelComposite).then((folder) => {
+			getModelFolder(currentProject, selectedModelComposite).then((folder) => {
 
-            uploadFilesToModelFolder(currentProject, folder, files).then((uploadedFiles) => {
+				uploadFilesToModelFolder(currentProject, folder, files).then((uploadedFiles) => {
 
-               setBusy(false)
-               if (onFilesUploaded) onFilesUploaded(uploadedFiles)
+					setBusy(false)
+					if (onFilesUploaded) onFilesUploaded(uploadedFiles)
 
-            })
+				})
 
-         })
+			})
 
-      }
-      
-   }
+		}
 
-   return <ChooseFiles onAddFiles={uploadFiles} busy={busy} busyMsg={`Uploading ${currentFileCount} of ${totalFileCount} (${currentProgress}%)`}/>
+	}
+
+	return <ChooseFiles selectedElement={selectedElement} onAddFiles={uploadFiles} busy={busy} busyMsg={`Uploading ${currentFileCount} of ${totalFileCount} (${currentProgress}%)`} />
 
 }
 
