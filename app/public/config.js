290ef691fc4e91d4952a0d2572f817d2f559dd1a
ibrahimfirat-invicara <ibrahim.firat@invicara.com>
2025-07-14 16:15:06 +0300

    New App ID

--- app/public/config.js
+++ app/public/config.js
@@ -5,5 +5,5 @@ const endPointConfig = {
    datasourceServiceOrigin: 'https://sandbox-api.invicara.com',
    graphicsServiceOrigin: 'https://sandbox-api.invicara.com',
    baseRoot: 'http://localhost:8083',
-   applicationId: '859f8b75-0114-4f70-9edf-8fa8b8e640e5'
+   applicationId: '698c1b53-5343-4028-a259-f8d66399c36c'
 }
