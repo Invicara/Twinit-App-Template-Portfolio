a45d721ea647d3480c2da42eccb66b925a589e19
scottmollon-invicara <scott.mollon@invicara.com>
2025-07-30 14:14:13 -0400

    code docs

--- app/ipaCore/components/ModelDocs/modelDocUtils.js
+++ app/ipaCore/components/ModelDocs/modelDocUtils.js
@@ -1,7 +1,11 @@
 import { IafFileSvc, IafFile } from "@dtplatform/platform-api"
 
+// 1. creates the SHARED file folder in the file service if it doesnt yet exist
+// 2. creates a File Container in the Item Service for the File Servce RootContainer if it doesnt yet exist
+// 3. creates a SHARED File Container in the Item Service if it doesnt yet exist
 const createRootFolderAndContainer = async (project) => {
    try {
+      // creates the SHARED file folder in the file service if it doesnt yet exist
       const rootFolderSearchCriteria = {
          _namespaces: project._namespaces,
          _parents: 'root',
@@ -16,6 +20,7 @@ const createRootFolderAndContainer = async (project) => {
          sharedFolder = await IafFileSvc.addFolder('SHARED', project._namespaces, null);
       }
 
+      //creates a File Container in the Item Service for the File Servce RootContainer if it dosnt yet exist
       let rootContainer = await IafFile.getRootContainer(project);
 
       if (!rootContainer) {
@@ -25,6 +30,7 @@ const createRootFolderAndContainer = async (project) => {
          );
       }
 
+      //creates a SHARED File Container in the Item Service if it doesnt yet exist
       let sharedFileContainer = await IafFile.getContainers(project, { _name: "SHARED" });
 
       if (!sharedFileContainer) {
@@ -45,6 +51,8 @@ const createRootFolderAndContainer = async (project) => {
 
 // gets or creates a File Service folder in the root container
 // with the provided model's name
+// Also creates a NamedFileCollection container for file items
+// the file items are used to relate files to model elements
 export const getModelFolder = async (project, modelComposite) => {
 
    let modelFolder
