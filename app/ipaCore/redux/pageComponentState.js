3efb36c2fe08a7c89bfda05a28392d9c6be03504
Julio Pereira <julio.pereira.ext@invicara.com>
2025-09-02 18:50:53 +0200

    feature - allowing map types

--- app/ipaCore/redux/pageComponentState.js
+++ app/ipaCore/redux/pageComponentState.js
@@ -4,7 +4,8 @@ import {createSelector, createSlice} from '@reduxjs/toolkit'
 export const STATE_KEY = 'pageComponentState';
 
 const initialState = {
-    clickEvent: undefined
+    clickEvent: undefined,
+    mapTypes: {}
 }
 
 const pageComponentSlice = createSlice({
@@ -13,13 +14,16 @@ const pageComponentSlice = createSlice({
     reducers: {
         setClickEvent: (state, action) => {
             state.clickEvent = action?.payload
+        },
+        setMapTypes: (state, action) => {
+            state.mapTypes = action?.payload
         }
     }
 })
 
 
 
-export const { setClickEvent } = pageComponentSlice.actions
+export const { setClickEvent, setMapTypes } = pageComponentSlice.actions
 export default pageComponentSlice.reducer
 
 export const getSlice = (rootState) => rootState[STATE_KEY];
@@ -27,4 +31,9 @@ export const getSlice = (rootState) => rootState[STATE_KEY];
 export const getClickEvent = createSelector(
     getSlice,
     (slice) => slice.clickEvent
+)
+
+export const getMapTypes = createSelector(
+    getSlice,
+    (slice) => slice.mapTypes
 )
