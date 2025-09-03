6384005ba8e16a91a1f547e6271215943a8b140c
Julio Pereira <julio.pereira.ext@invicara.com>
2025-09-03 18:24:32 +0200

    fix - correcting site color and minimum close perimeter value

--- app/ipaCore/pageComponents/portfolioOverview/components/statePanels/SiteDetails.jsx
+++ app/ipaCore/pageComponents/portfolioOverview/components/statePanels/SiteDetails.jsx
@@ -457,8 +457,8 @@ export default function SiteDetails({ context }) {
                 Math.pow(newPin[1] - firstPin[1], 2)
             );
             
-            // Close shape if clicked within ~50 meters of first pin (approximate)
-            if (distance < 0.0005) {
+            // Close shape if clicked within ~25 meters of first pin (approximate)
+            if (distance < 0.00025) {
                 completeShape(newPins);
                 return;
             }
