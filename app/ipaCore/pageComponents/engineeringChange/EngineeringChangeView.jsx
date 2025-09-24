75f107b231667608acc380d05847e8cd07f07b57
Tikaram Charde <tikaram.charde@globallogic.com>
2025-09-24 15:24:41 +0530

    added the card and list page

--- app/ipaCore/pageComponents/engineeringChange/EngineeringChangeView.jsx
+++ app/ipaCore/pageComponents/engineeringChange/EngineeringChangeView.jsx
@@ -13,10 +13,7 @@ import {
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import Chip from '@material-ui/core/Chip';
-
+import ChangeList from './components/ChangeList.jsx';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -80,16 +77,16 @@ const useStyles = makeStyles((theme) => ({
     opacity: 1,
     color: '#5D5D5D',
   },
-  divider: {
-    border: 'none',
-    height: '1px',
-    borderTop: '1px solid #EBEBEB',
-    marginBottom: 24,
-    width: 532,
-    marginTop: 0,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
+  // divider: {
+  //   border: 'none',
+  //   height: '1px',
+  //   borderTop: '1px solid #EBEBEB',
+  //   marginBottom: 24,
+  //   width: 532,
+  //   marginTop: 0,
+  //   marginLeft: 'auto',
+  //   marginRight: 'auto',
+  // },
   root: {
     maxWidth: 400,
     border: '1px solid #e0e0e0', // Light grey border
@@ -137,99 +134,80 @@ export default function EngineeringChangeView({ currentState, context, userConfi
 
   return (
     <div>
-      <Box display='flex' justifyContent='left' pt={4} pl={4}>
-        <form onSubmit={handleSubmit}>
+      <Box display="flex" flexDirection="column" mr={2}>
+        <Box p={2} mb={2}>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="body1" className={classes.label}>Search Input</Typography>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  name="search"
+                  placeholder={labels.search}
+                  value={filters.search}
+                  onChange={handleChange}
+                  className={classes.textField}
+                  InputProps={{
+                    classes: { input: classes.inputText },
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon className={classes.searchIcon} />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1" className={classes.label}>Filter</Typography>
+                <FormControl
+                  variant="outlined"
+                  className={classes.formControl}
+                >
+                  <Select
+                    name="group"
+                    value={filters.group}
+                    onChange={handleChange}
+                    displayEmpty
+                    IconComponent={KeyboardArrowDownIcon}
+                    renderValue={(selected) => {
+                      if (!selected) {
+                        return <span className={classes.placeholder}>Filter by</span>;
+                      }
+                      return selected;
+                    }}
+                  >
+                    {/* TODO: hook real data into the menu items selection */}
+                    <MenuItem value="approved">Approved</MenuItem>
+                    <MenuItem value="registered">Registered</MenuItem>
+                    <MenuItem value="closed">Closed</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <br />
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  disabled={true}
+                >
+                  Search
+                </Button>
+              </Grid>
+
+            </Grid>
+          </form>
+        </Box>
+        <Box p={2} mb={2}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Typography variant="body1" className={classes.label}>Search Input</Typography>
-              <TextField
-                fullWidth
-                variant="outlined"
-                name="search"
-                placeholder={labels.search}
-                value={filters.search}
-                onChange={handleChange}
-                className={classes.textField}
-                InputProps={{
-                  classes: { input: classes.inputText },
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchIcon className={classes.searchIcon} />
-                    </InputAdornment>
-                  ),
-                }}
-              />
+              <ChangeList />
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="body1" className={classes.label}>Filter</Typography>
-              <FormControl
-                variant="outlined"
-                className={classes.formControl}
-              >
-                <Select
-                  name="group"
-                  value={filters.group}
-                  onChange={handleChange}
-                  displayEmpty
-                  IconComponent={KeyboardArrowDownIcon}
-                  renderValue={(selected) => {
-                    if (!selected) {
-                      return <span className={classes.placeholder}>Filter by</span>;
-                    }
-                    return selected;
-                  }}
-                >
-                  {/* TODO: hook real data into the menu items selection */}
-                  <MenuItem value="approved">Approved</MenuItem>
-                  <MenuItem value="registered">Registered</MenuItem>
-                  <MenuItem value="closed">Closed</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <br />
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                disabled={true}
-              >
-                Search
-              </Button>
-            </Grid>
-            
           </Grid>
-        </form>
+        </Box>
       </Box>
-    
-      {/* <Box display='flex' justifyContent='left' pt={4} pl={4}>
-        <Card className={classes.root}>
-          <CardContent>
-            <Chip label="Approved" className={classes.chip} />
-
-            <Typography variant="h6" className={classes.title}>
-              EC Title: Adjusted flow rate by +100 gpm
-            </Typography>
-
-            <Typography variant="body2" className={classes.detail}>
-              Base Revision: 000
-            </Typography>
-            <Typography variant="body2" className={classes.detail}>
-              Revision: 000A
-            </Typography>
-            <Typography variant="body2" className={classes.detail}>
-              EC Type: Technical Parameters
-            </Typography>
-            <Typography variant="body2" className={classes.detail}>
-              EC ID: 001
-            </Typography>
-            <Typography variant="body2" className={classes.detail}>
-              Date Reviewed: 25/08/2010
-            </Typography>
-          </CardContent>
-        </Card>
-      </Box> */}
     </div>
   );
 }
