b0e8e27bc3ca85962490dd8c40b946fe1c7b2a83
Adam O'Reilly <adam.reilly.ext@invicara.com>
2025-09-29 17:16:55 +0400

    treeSearch component

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -13,6 +13,7 @@ import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { ThemeProvider as ThemeProviderV5, CssBaseline as CssBaselineV5 } from '@mui/material';
 import { themeOptions } from '../styles/defaultTheme';
 import { themeOptions as themeOptionsV5 } from '../styles/defaultTheme_mui_v5';
+import { TreeProvider } from '../ipaCore/contexts/TreeContext'; 
 
 mmvRegisterMode("mmvGIS", mapboxGISMode)
 
@@ -56,7 +57,9 @@ const AppWithModelContext = () => {
 const container = document.getElementById('app')
 const root = createRoot(container)
 root.render(<AliveScope>
-       <AppWithModelContext />
+          <TreeProvider>
+          <AppWithModelContext />
+        </TreeProvider>
     </AliveScope>)
 
 if (module.hot) {
