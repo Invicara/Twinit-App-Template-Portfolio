e6bea357bc2170c3d6ca02e59628dfea8dbed0bb
Julio Pereira <julio.pereira.ext@invicara.com>
2025-10-02 12:44:46 +0200

    fix - using structure to allow model drill down

--- app/ipaCore/pageComponents/portfolioOverview/components/statePanels/building/BuildingInfo.jsx
+++ app/ipaCore/pageComponents/portfolioOverview/components/statePanels/building/BuildingInfo.jsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
 import BuildingDetails from "./BuildingDetails.jsx";
 import ModelDetails from "./ModelDetails.jsx";
 import {useContext, useEffect, useState} from "react";
@@ -12,34 +10,38 @@ import {ModelContext} from "../../../../../contexts/ModelContext.js";
 import {Box} from "@material-ui/core";
 import CustomButton from "../../../../../components/atoms/CustomButton.jsx";
 import {MapMachineContext} from "../../../PortfolioOverview.jsx";
+import { useSelector } from 'react-redux';
+import { getStructures } from '../../../../../redux/pageComponentState.js';
 
 export default function BuildingInfo({context}) {
     const { data = [], siteId, buildingId } = context;
     const { building: buildings = [] } = data;
     const currentBuilding = buildings.find(b => b.buildingId == buildingId);
+    const structures = useSelector(getStructures);
+    const currentStructure = (structures|| {})[currentBuilding?.structureName];
     const { selectedModelComposite, availableModelComposites, setSelectedModelComposite } = useContext(ModelContext);
     const { send, actor } = useContext(MapMachineContext);
     const [model, setModel] = useState({});
 
     useEffect(() => {
         // Find and set the model composite based on building's ModelName
-        if (availableModelComposites && currentBuilding?.ModelName) {
+        if (availableModelComposites && currentStructure?.modelName) {
             const matchingModel = availableModelComposites.find(
-                model => model._name === currentBuilding.ModelName ||
-                    model._name.includes(currentBuilding.ModelName) ||
-                    currentBuilding.ModelName.includes(model._name)
+                model => model._name === currentStructure.modelName ||
+                    model._name.includes(currentStructure.modelName) ||
+                    currentStructure.modelName.includes(model._name)
             );
 
             if (matchingModel) {
                 console.log('Setting model composite lacally:', matchingModel);
                 setModel(matchingModel);
             } else {
-                console.log('No matching model found for:', currentBuilding.ModelName);
+                console.log('No matching model found for:', currentStructure.modelName);
                 console.log('Available models:', availableModelComposites.map(m => m._name));
                 setModel()
             }
         }
-    }, [currentBuilding]);
+    }, [currentBuilding, currentStructure]);
 
     const handleSelectModel = () => {
         // Example modelElementId - you can modify this based on your needs
