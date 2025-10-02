e5d18db6a2b3f9ad59ff58e81ee08276df1d5bab
dominika <dominika.oles@invicara.com>
2025-10-02 17:34:19 +0100

    sync markers tracking with mmv commands

--- app/client/scripts/mapEntryActions.mjs
+++ app/client/scripts/mapEntryActions.mjs
@@ -9,6 +9,7 @@ import {v4 as uuid} from "uuid"
 import bbox from "@turf/bbox";
 import centroid from "@turf/centroid";
 import {
+    addMarkers,
     clearStaleMarkers,
     clearStaleMarkersByPath,
     makeMarkerShell,
@@ -1805,7 +1806,9 @@ async function handleMarkers(stateValue, markersConfig, {context, self}) {
                         }
                     }]
                     context.mmvSend(commands);
-                    clearStaleMarkers(allFeaturesMarkerIds);
+                    graphics.forEach(graphic => {
+                        addMarkers(graphic);//track markers internally
+                    })
                 }
 
 
