49017739c25299504fa3a25e9558500d4cdb1211
Cesar Fraisseix <cesar.fraisseix@invicara.com>
2025-10-06 22:30:20 +0200

    Updated Terminology

--- app/ipaCore/pageComponents/portfolioOverview/PortfolioOverview.jsx
+++ app/ipaCore/pageComponents/portfolioOverview/PortfolioOverview.jsx
@@ -90,7 +90,7 @@ export const MapMachineContext = createContext();
 export const MapContext = createContext();
 const DEFAULT_PATHS = [
     [
-        { displayName: "Portfolio", state: 'portfolio', idKey: null, scopeLevel: 0 },
+        { displayName: "Fleet", state: 'portfolio', idKey: null, scopeLevel: 0 },
         { displayName: "Facility", state: 'site', idKey: 'siteId', feature: "polygon", api: "site/all", scopeLevel: 1, collShortName: "geo_sites_coll", parentState: "portfolio" },
         { displayName: "Unit", state: 'building', idKey: 'buildingId', feature: "mesh", api: "building/all", scopeLevel: 2, collShortName: "building_coll", parentState: "site" },
         { displayName: "Equipments", state: 'modelElement', idKey: 'modelElementId', scopeLevel: 3, parentState: "building" },
