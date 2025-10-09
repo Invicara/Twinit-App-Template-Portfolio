2b6f8c1820b9f3146fc9c572309df347aed80b04
jonw-ivcr <jonathan.wong@invicara.com>
2025-10-09 16:52:50 +0100

    HIT-128: Remove redundant endpoint

--- setup/template packages/dataModel/scripts/omapiTests.mjs
+++ setup/template packages/dataModel/scripts/omapiTests.mjs
@@ -349,7 +349,6 @@ let scriptModule = {
          `${baseOmapiUrl}/siteequip/facilities/A/units`,
          `${baseOmapiUrl}/siteequip/facilities/B/units/02/systems`,
          `${baseOmapiUrl}/siteequip/facilities/A/units/02/systems/RCS`,
-         `${baseOmapiUrl}/siteequip/facilities/A/units/01/equipments/RCP-A-011/ecs`,
          `${baseOmapiUrl}/siteequip/facilities/B/units/02/systems/RCS/equipmenttypes`,
          `${baseOmapiUrl}/siteequip/facilities/B/units/02/systems/RCS/equipmenttypes/Pump/equipment`
          `${baseOmapiUrl}/siteequip/facilities/A/units/01/equipments/ecs?ids=RCP-A-011,RCP-A-012,RCP-A-013`,
