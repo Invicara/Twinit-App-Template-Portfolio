d9fee03290c15937c917eca080e40d7f3c788ff4
Adam O'Reilly <adam.reilly.ext@invicara.com>
2025-11-03 13:12:45 +0400

    replacing mui dialog component

--- app/ipaCore/components/dialog/HomepageDialog.jsx
+++ app/ipaCore/components/dialog/HomepageDialog.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
   Button,
-  Typography,
 } from "@mui/material";
 
+import './HomepageDialog.scss'
 
 const HomepageDialog = ({ dialogOpen, setDialogOpen }) => {
 
@@ -17,36 +13,35 @@ const HomepageDialog = ({ dialogOpen, setDialogOpen }) => {
     }
  
     return (
-        <>
-            <Dialog open={dialogOpen} onClose={handleDialogClose}>
-                <DialogTitle>Welcome!</DialogTitle>
-                <DialogContent>
-                    <Typography variant="body1">
-                        The Fleet Management Proof of Concept (POC) was produced jointly by Twinit and Hitachi to showcase the flexibility and scalability of the Twinit platform and the speed at which Twinit development can occur. This POC was created based on Hitachi use cases and is the product of a single two week development sprint. As a result of the focus on delivering the defined user journeys, the POC is not a complete application and has not undergone full release testing. Features may be incomplete and issues may occur. Moving forward toward an MVP, these would be addressed.
-                    </Typography>
-                </DialogContent>
-                <DialogActions>
-                    <Button 
-                        onClick={handleDialogClose} 
-                        variant="contained" 
-                        color="primary" 
-                        sx={{
+
+        <div className="dialog-backdrop">
+            <div className="dialog-container">
+                <h2 className="dialog-title">Welcome</h2>
+                <p className="dialog-message">
+                    The Fleet Management Proof of Concept (POC) was produced jointly by Twinit and Hitachi to showcase the flexibility and scalability of the Twinit platform and the speed at which Twinit development can occur. This POC was created based on Hitachi use cases and is the product of a single two week development sprint. As a result of the focus on delivering the defined user journeys, the POC is not a complete application and has not undergone full release testing. Features may be incomplete and issues may occur. Moving forward toward an MVP, these would be addressed.
+                </p>
+                <div className="dialog-actions">
+                <Button 
+                    onClick={handleDialogClose} 
+                    variant="contained" 
+                    color="primary" 
+                    sx={{
+                        backgroundColor: "#DF158C",
+                        color: "white",
+                        textTransform: "none",
+                        "&:focus": {
                             backgroundColor: "#DF158C",
-                            color: "white",
-                            textTransform: "none",
-                            "&:focus": {
-                                backgroundColor: "#DF158C",
-                            },
-                            "&:hover": {
-                                backgroundColor: "#C71784",
-                            }
-                        }}
-                    >
-                        Accept
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
+                        },
+                        "&:hover": {
+                            backgroundColor: "#C71784",
+                        }
+                    }}
+                >
+                    Accept
+                </Button>
+                </div>
+            </div>
+        </div>
     )
 }
 
