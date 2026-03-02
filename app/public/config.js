2776bf6f1e08d71b318f45386cb2ee4dce575bd3
Cesar Fraisseix <cesar.fraisseix@invicara.com>
2026-03-02 13:15:06 +0100

    Set config.js back for development

--- app/public/config.js
+++ app/public/config.js
@@ -1,10 +1,10 @@
 const endPointConfig = {
-   itemServiceOrigin: 'https://api.invicara.com',
-   passportServiceOrigin: 'https://api.invicara.com',
-   fileServiceOrigin: 'https://api.invicara.com',
-   datasourceServiceOrigin: 'https://api.invicara.com',
-   graphicsServiceOrigin: 'https://api.invicara.com',
-   objectModelServiceOrigin: 'https://api.invicara.com',
-   baseRoot: 'https://portfolio.invicara.io',
+   itemServiceOrigin: 'https://sandbox-api.invicara.com',
+   passportServiceOrigin: 'https://sandbox-api.invicara.com',
+   fileServiceOrigin: 'https://sandbox-api.invicara.com',
+   datasourceServiceOrigin: 'https://sandbox-api.invicara.com',
+   graphicsServiceOrigin: 'https://sandbox-api.invicara.com',
+   objectModelServiceOrigin: 'https://sandbox-api.invicara.com',
+   baseRoot: 'http://localhost:8083',
    applicationId: '698c1b53-5343-4028-a259-f8d66399c36c'
 }
